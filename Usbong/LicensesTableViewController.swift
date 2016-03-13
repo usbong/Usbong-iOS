f0a0f175d8688fab92c41244ea2eddee8ea247ad
Chris Amanse <christopheramanse@gmail.com>
2016-03-13 23:18:29 +0800

    Create LicensesTableViewController
    - connect code to storyboard
    - add reuse identifier for prototype cell

---
+++ Usbong/LicensesTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LicensesTableViewController.swift
+//  Usbong
+//
+//  Created by Joe Amanse on 13/03/2016.
+//  Copyright © 2016 Usbong Social Systems, Inc. All rights reserved.
+//
+
+import UIKit
+
+class LicensesTableViewController: UITableViewController {
+    let licenses = ["Crypto", "SWXMLHash", "UsbongKit", "ZipArchive"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return licenses.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = licenses[indexPath.row]
+        
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
