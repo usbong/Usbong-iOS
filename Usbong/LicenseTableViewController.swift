7e646fda941166fe24289bd29b19f5ec9934fb56
Chris Amanse <christopheramanse@gmail.com>
2016-03-13 23:25:16 +0800

    Create LicenseTableViewController
    - connect code to storyboard

---
+++ Usbong/LicenseTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LicenseTableViewController.swift
+//  Usbong
+//
+//  Created by Joe Amanse on 13/03/2016.
+//  Copyright © 2016 Usbong Social Systems, Inc. All rights reserved.
+//
+
+import UIKit
+
+class LicenseTableViewController: UITableViewController {
+    var license: String? {
+        didSet {
+            navigationItem.title = title ?? "Unkown"
+            tableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = "No License Found"
+        
+        return cell
+    }
+}
