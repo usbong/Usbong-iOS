a7269e389c846e326b5855ed611b61cdf409a1f9
Chris Amanse <christopheramanse@gmail.com>
2016-03-16 17:36:03 +0800

    Integrate fastlane/snapshot

--- UsbongUITests/UsbongUITests.swift
+++ UsbongUITests/UsbongUITests.swift
@@ -18,8 +18,11 @@ class UsbongUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -28,9 +31,31 @@ class UsbongUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testSnapshot() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons["Settings"].tap()
+        snapshot("3Settings")
+        
+        tabBarsQuery.buttons["Trees"].tap()
+        snapshot("0Trees")
+        
+        app.tables.staticTexts["Usbong iOS"].tap()
+        
+        // Wait until task node is available
+        while !app.tables.cells.childrenMatchingType(.TextView).element.exists {
+            sleep(1)
+        }
+        
+        snapshot("1Usbong iOS")
+        
+        app.buttons["NEXT"].tap()
+        
+        sleep(1)
+        snapshot("2Task Nodes")
     }
     
 }
